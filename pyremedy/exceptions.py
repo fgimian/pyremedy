0fa4a837ea9a1d5d4d27a20fc061356956a21e80
Frank Gimian <Frank.Gimian@team.telstra.com>
2014-07-17 15:19:08 +1000

    Reduced repetition in the ARS class by moving field extraction and updating into methods

--- pyremedy/exceptions.py
+++ pyremedy/exceptions.py
@@ -1,3 +1,8 @@
 class ARSError(Exception):
     """A generic high-level ARS exception"""
     pass
+
+
+class ARSDataTypeError(Exception):
+    """An error raised when an un-handled Remedy data type is encountered"""
+    pass
