1173d370a715a20ee06b47403a85150bf27c8f4c
Frank Gimian <Frank.Gimian@team.telstra.com>
2014-07-19 20:52:54 +1000

    Added a basic setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name='pyremedy',
+    version='0.1',
+    description='A simple remedy for remedy.',
+    packages=['pyremedy'],
+    zip_safe=False
+)
