c907faf65351cf1f1bc2e3db86080e726997e648
Frank Gimian <Frank.Gimian@team.telstra.com>
2014-07-20 15:42:44 +1000

    Bumping version to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyremedy',
-    version='0.1',
+    version='0.2',
     description='A simple remedy for remedy.',
     packages=['pyremedy'],
     zip_safe=False
