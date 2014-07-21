a9a636df3469c5c9449aa79813d02066666829ea
Fotis Gimian <fgimiansoftware@gmail.com>
2014-07-21 12:57:50 +1000

    Added the MIT license and further information to the setup.py file

--- setup.py
+++ setup.py
@@ -4,7 +4,29 @@ from setuptools import setup
 setup(
     name='pyremedy',
     version='0.3-dev',
+    url='https://github.com/fgimian/pyremedy',
+    license='MIT',
+    author='Fotis Gimian',
+    author_email='fgimiansoftware@gmail.com',
     description='A simple remedy for remedy.',
     packages=['pyremedy'],
-    zip_safe=False
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Software Development :: Libraries'
+    ]
 )
